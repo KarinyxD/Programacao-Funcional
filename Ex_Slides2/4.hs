c162b48e329a42aa0d57c8c3a96fc759986d6acc
Kariny Abrahao <kariny_abrahao@hotmail.com>
2025-04-15 08:16:20 -0300

    exs

--- Ex_Slides2/4.hs
+++ Ex_Slides2/4.hs
@@ -1,1 +1,15 @@
--- Transforme a função da questão 13 em um operador.
+-- Transforme a função da questão 1 em um operador.
+
+
+main = do 
+  putStrLn "Digite o primeiro valor (True ou False): "
+  a <- readLn
+  putStrLn "Digite o segundo valor (True ou False): "
+  b <- readLn
+  putStrLn ("O resultado de nAnd é: " ++ show(a `nAnd` b))
+
+-- precisa usar as crases para definir um novo operador.
+infix 3 `nAnd`
+nAnd :: Bool -> Bool -> Bool
+nAnd True True = False 
+nAnd _ _ = True
--- Ex_Slides2/5.hs
+++ Ex_Slides2/5.hs
@@ -1,1 +1,13 @@
 -- Crie um operador === compara um inteiro de um algarismo com um dígito. Ex.: 1 === '1' retorna True.
+
+main = do
+  putStr "Digite um inteiro de 1 dígito: "
+  num <- readLn
+  putStr "Digite o char: "
+  char <- getChar -- nao permite dar enter e o caractere de nova linha '\n' não é consumido e fica no buffer
+  putStrLn ("\nO resultado é: "++show (num === char))
+
+
+infix 4 ===
+(===) :: Int -> Char -> Bool
+num === char = num == read [char] 
